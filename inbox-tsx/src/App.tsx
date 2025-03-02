9edf2d53e1f3dc8b2c5df242923f99e5f3344ead
jimbohb <horvath.balazs.hu@gmail.com>
2025-03-02 23:15:21 +0100

    add material ui

--- inbox-tsx/src/App.tsx
+++ inbox-tsx/src/App.tsx
@@ -1,4 +1,3 @@
-/* eslint-disable react/prop-types */
 import { useState } from "react";
 import ContactsArea from "./ContactsArea/Index";
 import ChatContactCard from "./Components/ChatContactCard";
